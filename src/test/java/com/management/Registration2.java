7d4afa01af04d4344d5dd563c4ff77d14b6a29be
Aisha Zahid <ayeshazahid5896@gmail.com>
2025-12-18 19:07:19 +0500

    updated Registration2 automation code

--- src/test/java/com/management/Registration2.java
+++ src/test/java/com/management/Registration2.java
@@ -3,6 +3,9 @@ package com.management;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
@@ -18,7 +21,7 @@ public class Registration2 {
 	public static By PASSWORD_NAME_LOCATOR=By.id("password");
 	public static By REGISTER_BUTTON_LOCATOR=By.id("registerBtn");
 	
-	@Test
+	@Test (enabled=false)
 	
 public void form() throws InterruptedException
     { 
@@ -36,14 +39,148 @@ public void form() throws InterruptedException
      }
 
 
-
-//public void Registrationform()
-//{
-//	WebDriverWait wait = new WebDriverWait(Unique.driver,Duration.ofSeconds(10));
-//
-//	
-//	
+@Test
+public void Registrationform()
+{
+	//for firstname 
+	WebDriverWait wait = new WebDriverWait(Unique.driver,Duration.ofSeconds(10));
+try {
+	
+	WebElement FIRST_NAME_FIELD = wait.until(ExpectedConditions.presenceOfElementLocated(FIRST_NAME_LOCATOR));
+	if (FIRST_NAME_FIELD.isDisplayed() && FIRST_NAME_FIELD.isEnabled() ) {
+		FIRST_NAME_FIELD.clear();
+		FIRST_NAME_FIELD.sendKeys("AISHA");
+		
+	}
+	else {
+		System.out.println("Unable to find firstname locator");
+	}
+	
+	
+} catch (Exception e) {
+	// TODO: handle exception
+	System.out.println("Unable to find firstname locator");
 }
+	
+	
+//for last name
+
+try {
+	
+	WebElement LAST_NAME_FIELD = wait.until(ExpectedConditions.presenceOfElementLocated(LAST_NAME_LOCATOR));
+	if (LAST_NAME_FIELD.isDisplayed() && LAST_NAME_FIELD.isEnabled() ) {
+		LAST_NAME_FIELD.clear();
+		LAST_NAME_FIELD.sendKeys("ZAHID");
+		
+	}
+	else {
+		System.out.println("Unable to find lastname locator");
+	}
+	
+	
+} catch (Exception e) {
+	// TODO: handle exception
+	System.out.println("Unable to find lastname locator");
+}
+	
+//for phone
+try {
+	
+	WebElement PHONE_NUMBER_FIELD = wait.until(ExpectedConditions.presenceOfElementLocated(PHONE_NUMBER_LOCATOR));
+	if (PHONE_NUMBER_FIELD.isDisplayed() && PHONE_NUMBER_FIELD.isEnabled() ) {
+		PHONE_NUMBER_FIELD.clear();
+		PHONE_NUMBER_FIELD.sendKeys("0309");
+		
+	}
+	else {
+		System.out.println("Unable to find phone number locator");
+	}
+	
+	
+} catch (Exception e) {
+	// TODO: handle exception
+	System.out.println("Unable to find phone number locator");
+}
+	
+	// for country 
+try {
+	
+	WebElement COUNTRY_NAME_FIELD = wait.until(ExpectedConditions.presenceOfElementLocated(COUNTRY_NAME_LOCATOR));
+	if (COUNTRY_NAME_FIELD.isDisplayed() && COUNTRY_NAME_FIELD.isEnabled() ) {
+		Select COUNTRY_DROPDOWN = new Select(COUNTRY_NAME_FIELD);
+        COUNTRY_DROPDOWN.selectByValue("Pakistan");
+		
+	}
+	else {
+		System.out.println("Unable to find country name locator");
+	}
+	
+	
+} catch (Exception e) {
+	// TODO: handle exception
+	System.out.println("Unable to find country name locator");
+}
+	//for emailaddress
+try {
+	
+	WebElement EMAIL_ADDRESS_FIELD = wait.until(ExpectedConditions.presenceOfElementLocated(EMAIL_ADDRESS_LOCATOR));
+	if (EMAIL_ADDRESS_FIELD.isDisplayed() && EMAIL_ADDRESS_FIELD.isEnabled() ) {
+		EMAIL_ADDRESS_FIELD.clear();
+		EMAIL_ADDRESS_FIELD.sendKeys("aisha4568");
+		
+	}
+	else {
+		System.out.println("Unable to find email address locator");
+	}
+	
+	
+} catch (Exception e) {
+	// TODO: handle exception
+	System.out.println("Unable to find email addresslocator");
+}
+	
+	// for password
+try {
+	
+	WebElement PASSWORD_NAME_FIELD = wait.until(ExpectedConditions.presenceOfElementLocated(PASSWORD_NAME_LOCATOR));
+	if (PASSWORD_NAME_FIELD.isDisplayed() && PASSWORD_NAME_FIELD.isEnabled() ) {
+		PASSWORD_NAME_FIELD.clear();
+		PASSWORD_NAME_FIELD.sendKeys("3321");
+		
+	}
+	else {
+		System.out.println("Unable to find password name locator");
+	}
+	
+	
+} catch (Exception e) {
+	// TODO: handle exception
+	System.out.println("Unable to find password name locator");
+}
+	
+	//for register button  
+try {
+	
+	WebElement REGISTER_BUTTON_FIELD = wait.until(ExpectedConditions.presenceOfElementLocated(REGISTER_BUTTON_LOCATOR));
+	 REGISTER_BUTTON_FIELD .click();
+	    
+
+//// 
+//// // Print text for confirmation
+    System.out.println("click :" + REGISTER_BUTTON_FIELD );
+}
+catch (Exception e) {
+    System.out.println("Unable to locate or click first submitloginbtn: " + e.getMessage());
+}
+	
+
+
+	
+}	
+}	
+
+
+
 
 
 
